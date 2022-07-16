f88dac3582072a83e4667ebe6a71f1010656cf0d
BragaJoao <joaogbraga@hotmail.com.br>
2022-07-16 11:37:50 -0300

    chore: auth archives and routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDataBase = require('./database/database');
 const usersRoutes = require('./users/users.routes');
+const authRoute = require('./auth/auth.routes');
 const charactersRoutes = require('./characters/characters.routes');
 const swaggerRoute = require('./swagger/swagger.routes');
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/users/', usersRoutes);
+app.use('/auth/', authRoute);
 app.use('/characters/', charactersRoutes);
 app.use('/api-docs', swaggerRoute);
 
